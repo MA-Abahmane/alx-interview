11631dfb5366ee60c24c1e2c51f86e3893c31fc0
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-12 23:29:29 +0100

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,18 +5,6 @@
 only two operations in this file: Copy All and Paste. Given a number n, write
 a method that calculates the fewest number of operations needed to result in
 exactly n H characters in the file.
-
-Prototype: def minOperations(n)
-Returns an integer
-If n is impossible to achieve, return 0
-Example:
-
-n = 9
-
-H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste =>
-HHHHHH => Paste => HHHHHHHHH
-
-Number of operations: 6
 """
 
 
@@ -25,9 +13,6 @@ def minOperations(n: int) -> int:
     cnt = 0
     nxt = 'H'
     body = 'H'
-
-    if (n == 1):
-        return 1
 
     while (len(body) < n):
         if (n % len(body) == 0):
