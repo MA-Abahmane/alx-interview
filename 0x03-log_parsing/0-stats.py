b6a0e9ccccaea58efd08850ed09a8327d1b014f0
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-21 22:09:09 +0100

    Mission passed ✨

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,6 +5,8 @@ Write a script that reads stdin line by line and computes metrics:
 
 Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
  <status code> <file size> (if the format is not this one, the
+ 178.10.226.186 - [2023-12-21 21:58:06.848107] "GET /projects/260
+   HTTP/1.1" 405 33
  line must be skipped)
 After every 10 lines and/or a keyboard interruption (CTRL + C),
 print these statistics from the beginning:
@@ -25,59 +27,63 @@ Warning: In this sample, you will have random value - it’s normal
 import sys
 
 
-def function():
-    """ Process function """
+def printer(stats: dict, file_size: int) -> None:
+    """ Function to print statistics """
+    print("File size: {:d}".format(file_size))
+
+    for k, v in sorted(stats.items()):
+        if v:
+            print("{}: {}".format(k, v))
+
+
+def function() -> None:
+    """ Stdin processing function """
     # Initialize variables for file size and line count
-    filesize, count = 0, 0
+    line_count, file_size = 0, 0
     # List of status codes to track
     stat_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     # Dictionary to store count for each status code
-    stats = {k: 0 for k in stat_codes}
-
-    def print_stats(stats: dict, file_size: int) -> None:
-        """ Function to print statistics """
-        print("File size: {:d}".format(filesize))
-
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+    stat_count = {k: 0 for k in stat_codes}
 
     try:
         # Iterate through each line from stdin
         for line in sys.stdin:
             # Increment line count
-            count += 1
+            line_count += 1
 
-            # Split the line into a list of words
+            # Split the line by spaces into a list of words
             data = line.split()
+            # print(data)
 
             try:
                 # Attempt to extract the status code from the line
+                # Position; before last
                 status_code = data[-2]
 
                 # If the status code is in the list; update count
-                if status_code in stats:
-                    stats[status_code] += 1
+                if status_code in stat_count:
+                    stat_count[status_code] += 1
             except BaseException:
                 pass
 
             try:
                 # Attempt to extract the file size from the line and
                 # update total file size
-                filesize += int(data[-1])
+                file_size += int(data[-1])
             except BaseException:
                 pass
 
             # Print statistics every 10 lines
-            if count % 10 == 0:
-                print_stats(stats, filesize)
+            if line_count % 10 == 0:
+                # calling printer
+                printer(stat_count, file_size)
 
         # Print final statistics
-        print_stats(stats, filesize)
+        printer(stat_count, file_size)
 
     except KeyboardInterrupt:
         # Handle keyboard interruption and print final statistics
-        print_stats(stats, filesize)
+        printer(stat_count, file_size)
         raise
 
 
