501a39da34f38ae43b1b029cd1dcb42f3692ae35
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-21 21:47:37 +0100

    push mission

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,54 +2,86 @@
 
 """
 Write a script that reads stdin line by line and computes metrics:
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+ <status code> <file size> (if the format is not this one, the
+ line must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+    (see input format above)
+    Number of lines by status code:
+        possible status code: 200, 301, 400, 401, 403, 404, 405
+          and 500
+        if a status code doesn’t appear or is not an integer, don’t
+          print anything for this status code
+        format: <status code>: <number>
+        status codes should be printed in ascending order
+Warning: In this sample, you will have random value - it’s normal
+ to not have the same output as this one.
 """
 
 import sys
-import re
 
 
-def output_ctrl(log: dict) -> None:
-    """ Helper function to display stats """
-    print("File size: {}".format(log["file_size"]))
-    
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+def function():
+    """ Process function """
+    # Initialize variables for file size and line count
+    filesize, count = 0, 0
+    # List of status codes to track
+    stat_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    # Dictionary to store count for each status code
+    stats = {k: 0 for k in stat_codes}
 
+    def print_stats(stats: dict, file_size: int) -> None:
+        """ Function to print statistics """
+        print("File size: {:d}".format(filesize))
 
-if __name__ == "__main__":
-    """ Import control """
-    # Regular expression to match the log format
-    regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
-    )
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
-    }
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
+        # Iterate through each line from stdin
         for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if match:
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+            # Increment line count
+            count += 1
 
-                # Update total file size
-                log["file_size"] += file_size
+            # Split the line into a list of words
+            data = line.split()
 
-                # Update status code frequency
-                if code.isdecimal():
-                    log["code_frequency"][code] += 1
+            try:
+                # Attempt to extract the status code from the line
+                status_code = data[-2]
 
-                # Print statistics every 10 lines
-                if line_count % 10 == 0:
-                    output_ctrl(log)
-    finally:
+                # If the status code is in the list; update count
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+
+            try:
+                # Attempt to extract the file size from the line and
+                # update total file size
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+
+            # Print statistics every 10 lines
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+
         # Print final statistics
-        output_ctrl(log)
+        print_stats(stats, filesize)
+
+    except KeyboardInterrupt:
+        # Handle keyboard interruption and print final statistics
+        print_stats(stats, filesize)
+        raise
+
+
+if __name__ == '__main__':
+    """ Import control """
+    # Call the function when the script is executed
+    function()
