9beb01f9d4cdad124ce141f61c6afff535af542b
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-22 15:23:45 +0100

    Update Task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,12 +12,12 @@ def rotate_2d_matrix(matrix):
     """ rotate 2D Matrix 90 degrees clockwise
     """
     l = len(matrix)
-    
+
     # Swap Rows to Columns
     for i in range(l):
         for j in range(i, l):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
+
     for lst in matrix:
         lst.reverse()
 
