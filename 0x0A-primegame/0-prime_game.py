520477fc517dba1942a1012817be0a864d997626
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-15 02:33:42 +0100

    Update task: Pycodestyled

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-""" Prime Game: Maria and Ben are playing a game """
+"""
+    > Prime Game <
+    Maria and Ben are playing a game. Given a set of consecutive integers starting
+    from 1 up to and including n, they take turns choosing a prime number from the
+    set and removing that number and its multiples from the set. The player that
+    cannot make a move loses the game.
+"""
 
 
 def isWinner(x, nums):
